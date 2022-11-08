cafba9b0289e8a57341ff069bdcc5a1e6a71c366
tkmktkhr <mukku.wordpress@gmail.com>
2022-11-09 07:41:24 +0900

    tiny fix test

--- src/utils/util.test.ts
+++ src/utils/util.test.ts
@@ -4,8 +4,8 @@ describe('array test', () => {
   describe('Utility', () => {
     test.each([
       { name: 'name', expectedName: 'name' },
-      { name: 'name', expectedName: 'name' },
-      { name: 'name', expectedName: 'name' },
+      { name: 'name2', expectedName: 'name2' },
+      { name: 'name3', expectedName: 'name3' },
     ])('parseNumber() : %p', ({ name, expectedName }) => {
       expect(name).toEqual(expectedName);
     });
