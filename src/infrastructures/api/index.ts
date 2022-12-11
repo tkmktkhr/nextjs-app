69a12d9aa2f237da24762a0e203fe92983c14fb0
tkmktkhr <mukku.wordpress@gmail.com>
2022-12-12 07:54:43 +0900

    change axios version - install some types

--- src/infrastructures/api/index.ts
+++ src/infrastructures/api/index.ts
@@ -1,12 +1,23 @@
-import axios, { AxiosInstance, AxiosResponse, AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
+import axios, {
+  AxiosInstance,
+  AxiosResponse,
+  AxiosRequestConfig,
+  AxiosHeaders,
+  AxiosRequestHeaders,
+  CreateAxiosDefaults,
+} from 'axios';
 
 export type TErrorResponse = { err: Error; status: number; isError: true };
 
-const headers: AxiosRequestHeaders = {
-  'x-api-key': 'api key',
+const apiKey = 'api-key';
+
+const headersList = {
+  'x-api-key': apiKey,
 };
 
-const config: AxiosRequestConfig = {
+const headers = new AxiosHeaders(headersList);
+
+const config: CreateAxiosDefaults = {
   // baseURL: 'http://127.0.0.1:3001',
   baseURL: 'http://host.docker.internal:3001',
   timeout: 5000,
