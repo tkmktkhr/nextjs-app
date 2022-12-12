f21366847d847663f27b81d11277468f62d76df7
tkmktkhr <mukku.wordpress@gmail.com>
2022-12-13 08:14:20 +0900

    fix axios header type inference

--- src/infrastructures/api/index.ts
+++ src/infrastructures/api/index.ts
@@ -1,28 +1,23 @@
-import axios, {
-  AxiosInstance,
-  AxiosResponse,
-  AxiosRequestConfig,
-  AxiosHeaders,
-  AxiosRequestHeaders,
-  CreateAxiosDefaults,
-} from 'axios';
+import axios, { AxiosInstance, AxiosResponse, CreateAxiosDefaults } from 'axios';
 
 export type TErrorResponse = { err: Error; status: number; isError: true };
 
-const apiKey = 'api-key';
+// const apiKey = 'api-key';
 
-const headersList = {
-  'x-api-key': apiKey,
-};
+// const headersList = {
+//   'x-api-key': apiKey,
+// };
 
-const headers = new AxiosHeaders(headersList);
+// const headers = new AxiosHeaders(headersList);
 
 const config: CreateAxiosDefaults = {
   // baseURL: 'http://127.0.0.1:3001',
   baseURL: 'http://host.docker.internal:3001',
   timeout: 5000,
   responseType: 'json',
-  headers,
+  headers: {
+    'x-api-key': 'api-key',
+  },
 };
 
 export class APIClient {
