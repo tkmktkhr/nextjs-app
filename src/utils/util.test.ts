738f31001ab83786a02d1adca7f07c7f10d267eb
tkmktkhr <mukku.wordpress@gmail.com>
2023-01-03 10:43:22 +0900

    rm cmt unit test case

--- src/utils/util.test.ts
+++ src/utils/util.test.ts
@@ -5,7 +5,6 @@ describe('array test', () => {
     test.each([
       { name: 'name', expectedName: 'name' },
       { name: 'name2', expectedName: 'name2' },
-      // { name: 'name3', expectedName: 'name3' },
     ])('parseNumber() : %p', ({ name, expectedName }) => {
       expect(name).toEqual(expectedName);
     });
