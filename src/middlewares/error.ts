ac93259db93daab0d8270843ac72dc58b68d81b8
tkmktkhr <takamuku.takahiro@gmail.com>
2023-02-11 11:49:02 +0900

    add not found error

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -5,3 +5,12 @@ export class UnauthorizedError extends Error {
     this.message = msg ?? this.name;
   }
 }
+
+export class NotFoundError extends Error {
+  constructor(msg?: string) {
+    super();
+    // this.code = 404;
+    this.name = new.target.name;
+    this.message = msg ?? this.name;
+  }
+}
