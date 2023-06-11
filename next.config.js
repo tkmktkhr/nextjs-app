9dd94baf6205ca7641d86f621af0af73c1e07c6f
tkmktkhr <mukku.wordpress@gmail.com>
2023-06-11 17:30:43 +0900

    add env

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
     // env will not be loaded even they are set env:{} part here or other env setting.
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    NEXT_PUBLIC_LOG_LEVEL: process.env.NEXT_PUBLIC_LOG_LEVEL,
   },
 };
 
